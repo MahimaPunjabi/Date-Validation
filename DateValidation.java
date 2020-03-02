b29bf58c0b4052491343669be74fc47790626bb6
Mahima Punjabi <mahimapunjabi27@gmail.com>
2020-03-02 10:58:17 +0530

    Add files via upload

---
+++ DateValidation.java
@@ -0,0 +1,29 @@
+
+import java.util.Date.*;
+
+public class DateValidation
+{
+    public static void main(String[] args) 
+    {
+      
+       int length=1;
+       int asciivalues=45;
+       int num;
+       for(int i=0; i<=length; i++)
+       {
+//           if(!(asciivalues <= 47))
+//                   {
+//                       System.out.println("-");
+//                   }
+//           else
+//           {
+//               System.out.println(" ");
+//           }
+           
+           System.out.println("Date:- 31/07/2018");
+       }
+       
+    }
+}
+
+     
